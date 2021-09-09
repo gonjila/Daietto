96c17f084d5ef29faf409b47826d044dd26982fb
gonjila <gonjilashvili.lasha@yahoo.com>
2021-09-10 03:16:19 +0400

    made responsible deliciosas section

--- src/components/deliciosas/PicturesComponent.jsx
+++ src/components/deliciosas/PicturesComponent.jsx
@@ -81,65 +81,66 @@ const Pictures = styled.div`
     position: relative;
 
     #loginScreenWrapper {
-        transform: translateY(-100px);
+        transform: translateY(-4.942vw);
         position: relative;
 
         #loginScreen {
-            width: 288px;
-            height: 624px;
+            width: 14.236vw;
+            height: 30.845vw;
             border-radius: 30px;
-            background: url(images/loginScreen.png) no-repeat center center;
-            box-shadow: 0px 0px 99px rgba(42, 90, 123, 0.23);
+            background: url(images/loginScreen.png) no-repeat center;
+            background-size: 200%;
+            box-shadow: 0px 0px 4.894vw rgba(42, 90, 123, 0.23);
             position: relative;
             z-index: 5;
         }
 
         &::after {
             content: '';
-            width: 409px;
-            height: 409px;
+            width: 20.213vw;
+            height: 20.213vw;
             border-radius: 50%;
             background: #d0f0d6;
             position: absolute;
-            top: 110px;
-            left: -60px;
+            top: 5.436vw;
+            left: -2.965vw;
             z-index: 1;
         }
     }
 
     #information {
-        width: 322px;
-        height: 103px;
+        width: 15.913vw;
+        height: 5.09vw;
         text-align: center;
         border-radius: 10px;
         background: #fff;
-        box-shadow: 0px 10px 20px rgba(25, 22, 109, 0.07);
+        box-shadow: 0px 0.494vw 0.988vw rgba(25, 22, 109, 0.07);
         position: absolute;
-        top: -65px;
-        left: 250px;
+        top: -3.212vw;
+        left: 12.355vw;
         z-index: 10;
 
         #nutritionalTitle {
             font-weight: 500;
-            font-size: 13px;
-            line-height: 24px;
+            font-size: 0.642vw;
+            line-height: 1.186vw;
             color: #23233c;
-            margin: 15px 0;
+            margin: 0.741vw 0;
         }
         #details {
-            margin: 20px 30px 0;
+            margin: 0.988vw 1.483vw 0;
             display: flex;
             justify-content: space-around;
 
             div {
                 .detailNumber {
                     font-weight: bold;
-                    font-size: 19px;
+                    font-size: 0.939vw;
                     color: #23233c;
                 }
                 .detailText {
                     font-weight: normal;
-                    font-size: 7px;
+                    font-size: 0.346vw;
                     color: #9e9e9e;
                 }
             }
@@ -153,41 +154,42 @@ const Pictures = styled.div`
     }
 
     #homeScreenWrapper {
-        margin-left: 80px;
-        transform: translateY(50px);
+        margin-left: 3.954vw;
+        transform: translateY(2.471vw);
         position: relative;
 
         #homeScreen {
-            width: 288px;
-            height: 624px;
+            width: 14.233vw;
+            height: 30.838vw;
             border-radius: 30px;
-            background: url(images/homeScreen.png) no-repeat center center;
-            box-shadow: 0px 3px 80px rgba(42, 90, 123, 0.23);
+            background: url(images/homeScreen.png) no-repeat center;
+            background-size: 200%;
+            box-shadow: 0px 0.148vw 3.954vw rgba(42, 90, 123, 0.23);
         }
     }
 
     #weekResult {
-        width: 394px;
-        height: 192px;
+        width: 19.471vw;
+        height: 9.489vw;
         border-radius: 10px;
         background: #fff;
-        box-shadow: 0px 10px 20px rgba(25, 22, 109, 0.07);
+        box-shadow: 0px 0.494vw 0.988vw rgba(25, 22, 109, 0.07);
         position: absolute;
-        bottom: 200px;
-        right: -100px;
+        bottom: 9.884vw;
+        right: -4.942vw;
 
         display: flex;
 
         #resultTextWrapper {
             text-align: center;
-            margin: 0 15px 0 40px;
+            margin: 0 0.741vw 0 1.977vw;
 
             #resultsTitle {
                 font-weight: bold;
-                font-size: 20px;
-                line-height: 26px;
+                font-size: 0.988vw;
+                line-height: 1.285vw;
                 color: #23233c;
-                margin: 40px 0 12px;
+                margin: 1.977vw 0 0.593vw;
             }
 
             #informationWrapper {
@@ -196,14 +198,14 @@ const Pictures = styled.div`
 
                 .resultText {
                     font-weight: normal;
-                    font-size: 10px;
-                    line-height: 15px;
+                    font-size: 0.494vw;
+                    line-height: 0.741vw;
                     color: #23233c;
                 }
                 .resultNumber {
                     font-weight: bold;
-                    font-size: 19px;
-                    line-height: 34px;
+                    font-size: 0.939vw;
+                    line-height: 1.68vw;
                     color: #6cc57c;
                 }
             }
@@ -213,25 +215,25 @@ const Pictures = styled.div`
             width: 38.5%;
 
             #dots {
-                margin: 20px 35px 0 0;
+                margin: 0.988vw 1.73vw 0 0;
 
                 display: flex;
-                gap: 5px;
+                gap: 0.247vw;
                 justify-content: flex-end;
 
                 .dot {
-                    width: 5px;
-                    height: 5px;
+                    width: 0.247vw;
+                    height: 0.247vw;
                     background: #7f8da0;
                     border-radius: 50%;
                 }
             }
 
             #picturesComponentGraph {
-                width: 81.98px;
-                height: 81.98px;
+                width: 4.051vw;
+                height: 4.051vw;
                 border-radius: 50%;
-                margin: 23px 30px 30px 30px;
+                margin: 1.137vw 1.483vw 1.483vw;
                 background-image: linear-gradient(225deg, #6cc57c 14%, #dcebf5 90%);
                 position: relative;
 
@@ -241,17 +243,17 @@ const Pictures = styled.div`
 
                 #graphText {
                     font-weight: bold;
-                    font-size: 15px;
+                    font-size: 0.741vw;
                     text-align: center;
-                    line-height: 16px;
+                    line-height: 0.791vw;
                     color: #23233c;
                     z-index: 10;
                 }
 
                 &:after {
                     content: '';
-                    width: 72px;
-                    height: 72px;
+                    width: 3.558vw;
+                    height: 3.558vw;
                     border-radius: 50%;
                     background: #fff;
                     position: absolute;
@@ -260,8 +262,8 @@ const Pictures = styled.div`
 
             #bottomWorldsInPicturesComponent {
                 font-weight: normal;
-                font-size: 10px;
-                line-height: 19px;
+                font-size: 0.494vw;
+                line-height: 0.939vw;
 
                 span {
                     color: #6cc57c;
@@ -270,14 +272,14 @@ const Pictures = styled.div`
         }
 
         #resultArrow {
-            width: 46px;
-            height: 46px;
+            width: 2.273vw;
+            height: 2.273vw;
             background: #fff;
-            filter: drop-shadow(0px 5px 30px rgba(8, 21, 81, 0.31));
+            filter: drop-shadow(0px 0.247vw 1.483vw rgba(8, 21, 81, 0.31));
             border-radius: 50%;
             position: absolute;
-            bottom: -6px;
-            left: -6px;
+            bottom: -0.297vw;
+            left: -0.297vw;
 
             display: flex;
             align-items: center;
